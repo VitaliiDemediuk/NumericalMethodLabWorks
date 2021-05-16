d31fa0efb1624e7a2dd15cb6df0aa91a2e883b83
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2021-05-16 23:28:06 +0300

    First commit for lab #4. Empty project.

---
+++ Lab#4/lab4_code/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using std::cout;
+
+void Task1(){
+
+}
+
+void Task2(){
+
+}
+
+void Task3(){
+
+}
+
+int main() {
+    Task1();
+    Task2();
+    Task3();
+    return 0;
+}
